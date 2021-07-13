7154fba964bc71b5ff7c9dab658431127cafee33
kevin <kevin345618@outlook.com>
2021-07-13 23:34:58 +0800

    make random food color

--- src/food.js
+++ src/food.js
@@ -1,5 +1,5 @@
 import { eatFood, expandSnake } from './snake.js'
-import { randomGridPosition } from './grid.js'
+import { randomGridPosition, randomGridColor } from './grid.js'
 
 const EXPANSION = 1
 let food = generateFood()
@@ -15,14 +15,16 @@ export function draw(gameboard) {
     const foodElement = document.createElement('div')
     foodElement.style.gridRowStart = food.y
     foodElement.style.gridColumnStart = food.x
-    foodElement.classList.add('food')
+    foodElement.classList.add('food', food.color)
     gameboard.appendChild(foodElement)
 }
 
 function generateFood() {
     let newFoodPosition
+    let newFoodColor
     while (newFoodPosition === undefined || eatFood(newFoodPosition)) {
         newFoodPosition = randomGridPosition()
+        newFoodColor = randomGridColor()
     }
-    return newFoodPosition
+    return { ...newFoodPosition, color: newFoodColor }
 }
--- src/grid.js
+++ src/grid.js
@@ -6,6 +6,16 @@ export function randomGridPosition() {
     }
 }
 
+export function randomGridColor() {
+    const colors = ['yellow', 'green', 'red']
+    const random = Math.floor(Math.random() * 10) // 0 - 9
+    let check = 0
+    if (random < 3) check = 0
+    else if (random >=3 && random < 6) check = 1
+    else check = 2
+    return colors[check]
+}
+
 export function outsideGrid(position) {
     const { x, y } = position
     return x < 1 || x > 25 || y < 1 || y > 25
